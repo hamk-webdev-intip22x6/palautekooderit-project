72cd286105a05cb2ef07344387e59d3784c94c1b
Niko Malinen <niko.malinen@student.hamk.fi>
2023-11-25 19:44:53 +0200

    deployinit

--- palautekooderit/wsgi.py
+++ palautekooderit/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'palautekooderit.settings')
+settings_module = 'palautekooderit.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'palautekooderit.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
