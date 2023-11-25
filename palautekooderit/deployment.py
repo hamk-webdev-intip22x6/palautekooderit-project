42f6f82843392850a25f1e8d4fe69cd867cbbc81
Niko Malinen <niko.malinen@student.hamk.fi>
2023-11-25 19:55:57 +0200

    unohinjotain

--- palautekooderit/deployment.py
+++ palautekooderit/deployment.py
@@ -2,6 +2,8 @@ import os
 from .settings.py import *
 from .settings import BASE_DIR
 
+
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ('WEBSITE_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ('WEBSITE_HOSTNAME')]
 DEBUG = False
@@ -21,15 +23,15 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_MYSQL_CONNECTIONSTRING']
-parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+conn_str = os.environ['AZURE_MYSQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'parameters['dbname']',
-        'HOST': 'parameters['host']',
-        'USER': 'parameters['user']',
-        'PASSWORD': 'parameters['password']',
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password'],
     }
 }
