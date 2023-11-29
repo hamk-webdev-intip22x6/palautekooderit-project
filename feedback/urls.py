3252e8235cca95672fe9d084272c382c10137b2e
Juhis <juho-pekka.hjelt@student.hamk.fi>
2023-11-29 00:52:38 +0000

    topic_create

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Feedback, TopicCreateView
 
 app_name = 'feedback'
 urlpatterns = [
     path('', views.FeedbackCreateView.as_view(), name='index'),
+    path('create-topic/', TopicCreateView.as_view(), name='create_topic'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls.base import reverse_lazy
-from .models import Feedback
+from .models import Feedback, Topic
 
 # Create your views here.
 class FeedbackCreateView(CreateView):
     model = Feedback
-    fields = ['topic', 'rating', 'good', 'bad']
+    fields = ['topic', 'rating', 'good', 'bad']  # Include all fields in the form
     template_name = "feedback/index.html"
     success_url = reverse_lazy('feedback:index')
+
+    def form_valid(self, form):
+        topic_name = form.cleaned_data['topic']
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        form.instance.topic = topic
+        return super().form_valid(form)
+
+class TopicCreateView(CreateView):
+    model = Topic
+    fields = ['name']
+    template_name = "feedback/create_topic.html"
+    success_url = reverse_lazy('feedback:index')  # Adjust the URL name based on your project structure
