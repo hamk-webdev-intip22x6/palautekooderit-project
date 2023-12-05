02da377be8dbae289f13ae1ee86627dd118b8ca2
Tturna <42873350+Tturna@users.noreply.github.com>
2023-12-05 17:27:30 +0200

    'create topic' button hidden from unauthorized users in feedback creation. Added some educational comments.

--- feedback/views.py
+++ feedback/views.py
@@ -7,15 +7,34 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 # Create your views here.
 class FeedbackCreateView(CreateView):
     model = Feedback
-    fields = ['topic', 'rating', 'good', 'bad', 'ideas']  # Include all fields in the form
+
+    # Include all fields except creator_user in the form
+    # creator_user is set automatically in the form_valid() method
+    fields = ['topic', 'rating', 'good', 'bad', 'ideas']
     template_name = "feedback/index.html"
     success_url = reverse_lazy('feedback:index')
 
+    # CreateView has a get_context_data() method that creates a context variable
+    # which contains data about the form and the current view.
+    # get_context_data() is automatically called when the view is rendering the template.
+    # We're overriding it here to add a variable that indicates whether the current user
+    # is a member of the topic_masters group.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_user = self.request.user
+        context['is_topic_master'] = current_user.groups.filter(name='topic_masters').exists()
+
+        return context
+
+    # This is called automatically when valid form data has been POSTed
     def form_valid(self, form):
         topic_name = form.cleaned_data['topic']
-        topic, created = Topic.objects.get_or_create(name=topic_name)
+        topic, is_created = Topic.objects.get_or_create(name=topic_name)
         form.instance.topic = topic
         form.instance.creator_user = self.request.user
+
+        # This saves the form instance to the database and redirects to the success_url
+        # This is default behavior, but we're overriding it to add the topic and creator_user
         return super().form_valid(form)
 
 class TopicCreateView(UserPassesTestMixin, CreateView):
